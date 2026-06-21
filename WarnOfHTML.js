cec4f6e8247387b24cecabd3d2ba3db9c4bad48e
BunnyTub <gadiel047@gmail.com>
2026-06-21 05:03:41 -0400

    Fix return issue

--- WarnOfHTML.js
+++ WarnOfHTML.js
@@ -1,13 +1,17 @@
-if (window.location.pathname.endsWith(".html")) {
-    const newPath = window.location.pathname.replace(/\.html$/, "");
-    const isLocal = window.location.protocol === "file:";
-    if (!isLocal) {
-        window.location.replace(newPath);
-        return;
+function RunSteps() {    
+    if (window.location.pathname.endsWith(".html")) {
+        const newPath = window.location.pathname.replace(/\.html$/, "");
+        const isLocal = window.location.protocol === "file:";
+        if (!isLocal) {
+            window.location.replace(newPath);
+            return;
+        }
     }
+
+    document.addEventListener("DOMContentLoaded", async () => {
+        const response = await fetch("/NavigationBar");
+        document.getElementById("menu").innerHTML = await response.text();
+    });
 }
 
-document.addEventListener("DOMContentLoaded", async () => {
-    const response = await fetch("/NavigationBar");
-    document.getElementById("menu").innerHTML = await response.text();
-});
+RunSteps();
