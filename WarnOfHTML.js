b3135ff4e00da6856d966f271aea14d862619cbc
BunnyTub <gadiel047@gmail.com>
2026-06-21 04:59:47 -0400

    Navigation bar test 2 (custom)

--- WarnOfHTML.js
+++ WarnOfHTML.js
@@ -3,5 +3,11 @@ if (window.location.pathname.endsWith(".html")) {
     const isLocal = window.location.protocol === "file:";
     if (!isLocal) {
         window.location.replace(newPath);
+        return;
     }
 }
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const response = await fetch("/NavigationBar");
+    document.getElementById("menu").innerHTML = await response.text();
+});
