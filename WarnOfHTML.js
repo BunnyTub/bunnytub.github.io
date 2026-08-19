1c5cf5c71de177ede3eb21247bc06bdb7b7640ae
BunnyTub <gadiel047@gmail.com>
2026-08-18 23:24:45 -0400

    no idea

--- WarnOfHTML.js
+++ WarnOfHTML.js
@@ -20,6 +20,29 @@ function RunSteps() {
             menuBar.style.display = "flex";
             //alert("Whoops, something is having trouble. Please reload the page!");
         }
+
+        document.querySelectorAll('.covered').forEach(function (el) {
+            el.insertAdjacentHTML('beforeend', '<cover></cover>');
+            el.addEventListener('mousedown', function (e) {
+                if (e.button === 2) {
+                    e.preventDefault();
+                    return false;
+                }
+                return true;
+            });
+
+            el.querySelectorAll('img').forEach(function (img) {
+                img.style.display = 'block';
+            });
+
+            el.addEventListener('mouseenter', function () {
+                var cover = el.querySelector('cover');
+                if (!cover) {
+                    el.innerHTML = '';
+                }
+            });
+        });
+
     });
 }
 
